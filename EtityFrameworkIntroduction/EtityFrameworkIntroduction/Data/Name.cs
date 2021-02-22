ea4609bae0da59a05ba6ba1b4374e70a6c2fafb2
trifon shterev <tshterev@abv.bg>
2021-02-22 21:01:27 +0200

    lectureCode

---
+++ EtityFrameworkIntroduction/EtityFrameworkIntroduction/Data/Name.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EntityFrameworkIntroduction.Data
+{
+    public partial class Name
+    {
+        public string FullName { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
---
+++ EtityFrameworkIntroduction/EtityFrameworkIntroduction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using EntityFrameworkIntroduction.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace EntityFrameworkIntroduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new SoftUniContext())
+            {
+                var towns = db.Towns
+                    .Include(t => t.Addresses)
+                    .ToList();
+
+                foreach (var town in towns)
+                {
+                    Console.WriteLine(town.Name);
+                    foreach (var address in town.Addresses)
+                    {
+                        Console.WriteLine($"---{address.AddressText}");
+                    }
+                }
+
+               
+            }
+        }
+    }
+}
